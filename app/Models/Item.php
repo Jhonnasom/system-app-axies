dfd4d06d52256d648deca12159c0ad0e5645461f
Jhonna <jhonna.somarriba@gmail.com>
2023-06-12 16:47:08 -0600

    Se creo la relacion de la tabla pivote de seguidores

---
+++ app/Models/Item.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    function collections():HasMany
+    {
+        return $this->hasMany(Collection::class);
+    }
+
+    function user():BelongsTo
+    {
+        return $this->hasMany(User::class);
+    }
+}
