2916de9b10a9ae2c5e3cf990b5e5b8a2c0177d88
Jhonna <jhonna.somarriba@gmail.com>
2023-06-16 21:10:48 -0600

    Category model se agrega relacion con items

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -15,4 +16,9 @@ class Category extends Model implements HasMedia
     protected $fillable = [
         'name'
     ];
+
+    function items():HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
 }
